730d0966612938f8ab870f14dee93c3db8b9ef99
Niladri Roy <niladriroyweb@outlook.com>
2022-02-06 23:21:14 +0600

    Add files via upload

---
+++ wp-config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Turn off all Unknown error for wordpress
+
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
